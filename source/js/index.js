f17dc8a71f4125eb2273f97b67615189c5381a86
Alexander Voronin <aovoronin.piano@gmail.com>
2022-08-24 19:40:47 +0300

    Finally done with menu on header

--- source/js/index.js
+++ source/js/index.js
@@ -5,4 +5,21 @@ if (currentLocation === 'catalog.html' || currentLocation === 'form.html') {
   headerLinks.forEach((link) => link.style.color = 'black');
 }
 
+const declineBtn = document.querySelector('.header__toggler');
+const navigation = document.querySelector('.nav');
+navigation.classList.add('nav--closed');
+
+declineBtn.addEventListener('click', () => {
+  navigation.classList.toggle('nav--closed');
+  if (declineBtn.classList.contains('active')) {
+    declineBtn.classList.remove('active');
+    declineBtn.classList.add('passive');
+  } else {
+    declineBtn.classList.remove('passive');
+    declineBtn.classList.add('active');
+  }
+
+});
+
+
 

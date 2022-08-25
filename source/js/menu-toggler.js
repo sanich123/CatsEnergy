59a4c46691a2f22c46d340841090a276753104e7
Alexander Voronin <aovoronin.piano@gmail.com>
2022-08-25 16:29:19 +0300

    Have done pixel perfect on header and promo in three resolutions

--- source/js/menu-toggler.js
+++ source/js/menu-toggler.js
@@ -1,19 +1,19 @@
 const menuToggler = document.querySelector('.header__toggler');
 const navigation = document.querySelector('.nav');
 const navClose = 'nav--closed';
-const hamburgerClass = 'header__toggler--hamburger';
+const openClass = 'header__toggler--open';
 const closeClass = 'header__toggler--close';
 navigation.classList.add(navClose);
 menuToggler.classList.remove(closeClass);
-menuToggler.classList.add(hamburgerClass);
+menuToggler.classList.add(openClass);
 
 menuToggler.addEventListener('click', () => {
   navigation.classList.toggle(navClose);
-  if (menuToggler.classList.contains(hamburgerClass)) {
-    menuToggler.classList.remove(hamburgerClass);
+  if (menuToggler.classList.contains(openClass)) {
+    menuToggler.classList.remove(openClass);
     menuToggler.classList.add(closeClass);
   } else {
     menuToggler.classList.remove(closeClass);
-    menuToggler.classList.add(hamburgerClass);
+    menuToggler.classList.add(openClass);
   }
 });
--- source/js/nav-color-changer.js
+++ source/js/nav-color-changer.js
@@ -1,6 +1,13 @@
 const currentLocation = document.location.href.slice(22);
 
 if (currentLocation === 'catalog.html' || currentLocation === 'form.html') {
-  const headerLinks = document.querySelectorAll('.nav-list__item a');
-  headerLinks.forEach((link) => link.style.color = 'black');
+  const headerLinks = document.querySelectorAll('.nav-list__item');
+
+  headerLinks.forEach((link) => {
+    if (link.classList.contains('nav-list__item--active')) {
+      link.firstChild.classList.add('green-stroke');
+    }
+    link.firstChild.style.color = 'black';
+  });
+
 }

6b71dbdf484576ff4b480deac76e925d50a6dbc6
sanich123 <aovoronin.piano@gmail.com>
2022-08-29 12:27:32 +0300

    Some work with svg continues

--- source/gulp/create-sprite.js
+++ source/gulp/create-sprite.js
@@ -5,7 +5,7 @@ import svgmin from 'gulp-svgmin';
 
 export const createSprite = () => {
   return gulp.src('source/img/svg/icons/*.svg')
-  // .pipe(svgmin())
+  .pipe(svgmin())
   .pipe(svgstore())
   .pipe(rename('icons.svg'))
   .pipe(gulp.dest('public/img/svg'));

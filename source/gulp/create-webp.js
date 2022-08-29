a2ecd50bb8db6b7fcb332c3df12a7f89d6c8da0a
sanich123 <aovoronin.piano@gmail.com>
2022-08-29 09:41:51 +0300

    Some improving

--- source/gulp/create-webp.js
+++ source/gulp/create-webp.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import gulpSquoosh from 'gulp-libsquoosh';
 
 export const createWebP = () => {
-  return gulp.src('source/img/**/*.{jpeg, png}')
+  return gulp.src('source/img/**/*.{jpeg,png}')
   .pipe(gulpSquoosh({
     webp: {},
   }))
